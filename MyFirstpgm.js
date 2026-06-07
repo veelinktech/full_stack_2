01bc79b7d7990da0018393380aadeeb1808937c6
veelinktech <104140334+veelinktech@users.noreply.github.com>
2026-06-07 09:46:02 +0530

    2nd commit
    Refactor code into functions for better structure

--- MyFirstpgm.js
+++ MyFirstpgm.js
@@ -1,5 +1,15 @@
-console.log("Welcome to JS")
-console.log(5+10)
-console.log(10-5)
-console.log(10*5)
-console.log(10/5)
+function disp()
+{
+  console.log("Welcome to JS")
+}
+
+function arithmetic()
+{
+  console.log(5+10)
+  console.log(10-5)
+  console.log(10*5)
+  console.log(10/5)
+}
+
+disp()
+arithmetic()
